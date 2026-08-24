c84f498a61e55f317a2953efdad41fa4d094db40
vinayak.g.hubballi <vinayak.g.hubballi@gmail.com>
2026-08-24 20:50:46 +0530

    saving the file to new branch

---
+++ example2.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter principal amount: "))
+interest = float(input("Enter interest %: "))
+time = float(input("Enter time in years: "))
+
+SI = (principal*interest*time)/100
+
+print("Simple Interest: ", SI)
